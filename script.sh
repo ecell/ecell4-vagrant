bb8b58b55f34f89218e803aab6b0e2ab7aa0f40c
Kozo Nishida <knishida@riken.jp>
2014-04-29 18:41:33 +0900

    use run_as_service branch

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-=======
 # Set up environment...
->>>>>>> run_as_service
+
 aptitude update
 aptitude install -y git g++ libboost-dev libboost-test-dev libboost-regex-dev libboost-random-dev libgsl0-dev libhdf5-serial-dev python-dev python-pip pkg-config cython python-zmq
 pip install ipython jinja2 tornado
@@ -13,13 +11,6 @@ cd /vagrant/ecell4/gillespie; python ../waf configure build install
 cd /vagrant/ecell4/ode; LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
 cd /vagrant/ecell4; PREFIX=/usr/local bash install.sh core_python gillespie_python ode_python
 
-<<<<<<< HEAD
-if [ ! -f "/root/.ipython/profile_nbserver/ipython_config.py" ]; then
-    ipython profile create nbserver; printf "%s\n" "c.NotebookApp.ip = '*'" "c.NotebookApp.open_browser = False" "c.NotebookApp.password = u''" "c.NotebookApp.port = 9999" >> /root/.ipython/profile_nbserver/ipython_config.py
-fi
-
-LD_LIBRARY_PATH=/usr/local/lib ipython notebook --profile=nbserver --port-retries=0 &
-=======
 # Put launch script of ipython notebook.
 chmod 755 /vagrant/start_ipython.sh
 
@@ -28,4 +19,3 @@ cp /vagrant/ipy.conf /etc/init/ipy.conf
 
 # Launch ipython service.
 initctl start ipy
->>>>>>> run_as_service
