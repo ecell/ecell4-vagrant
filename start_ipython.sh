3b98f8e8b40b11f4fc2cda1e63313ee61641b67c
Kozo Nishida <knishida@riken.jp>
2014-05-01 14:32:32 +0900

    start from ipynb directory

--- start_ipython.sh
+++ start_ipython.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-LD_LIBRARY_PATH=/usr/local/lib ipython notebook --notebook-dir='/vagrant' --no-browser --ip='*' --port 9999
+LD_LIBRARY_PATH=/usr/local/lib ipython notebook --notebook-dir='/vagrant/ecell4/ipynb' --no-browser --ip='*' --port 9999
