40ae46de30be35484fa8cc76c7f8874cfdcb60da
Kozo Nishida <knishida@riken.jp>
2014-05-01 19:05:39 +0900

    use optimize option

--- script.sh
+++ script.sh
@@ -6,11 +6,11 @@ aptitude update
 aptitude install -y git g++ libboost-dev libboost-test-dev libboost-regex-dev libboost-random-dev libgsl0-dev libhdf5-serial-dev python-dev python-matplotlib python-pip pkg-config cython python-zmq
 pip install ipython jinja2 tornado
 cd /vagrant; git clone git://github.com/ecell/ecell4; cd ecell4; git checkout -t origin/interface
-cd /vagrant/ecell4; PREFIX=/usr/local bash install.sh core
-cd /vagrant/ecell4/gillespie; LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
-cd /vagrant/ecell4/ode; LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
-cd /vagrant/ecell4/lattice; LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
-cd /vagrant/ecell4/bd; LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
+cd /vagrant/ecell4/core; CXXFLAGS="-Wall -O2" LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
+cd /vagrant/ecell4/gillespie; CXXFLAGS="-Wall -O2" LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
+cd /vagrant/ecell4/ode; CXXFLAGS="-Wall -O2" LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
+cd /vagrant/ecell4/lattice; CXXFLAGS="-Wall -O2" LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
+cd /vagrant/ecell4/bd; CXXFLAGS="-Wall -O2" LD_LIBRARY_PATH=/usr/local/lib python ../waf configure build install
 cd /vagrant/ecell4; PREFIX=/usr/local bash install.sh core_python gillespie_python ode_python lattice_python bd_python reaction_reader
 
 # Put launch script of ipython notebook.
