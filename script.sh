5e9c101c36ff660bf29cbb642b543eed46c217b1
Kozo Nishida <knishida@riken.jp>
2014-05-01 14:08:52 +0900

    add matplotlib deb

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # Set up environment...
 
 aptitude update
-aptitude install -y git g++ libboost-dev libboost-test-dev libboost-regex-dev libboost-random-dev libgsl0-dev libhdf5-serial-dev python-dev python-pip pkg-config cython python-zmq
+aptitude install -y git g++ libboost-dev libboost-test-dev libboost-regex-dev libboost-random-dev libgsl0-dev libhdf5-serial-dev python-dev python-matplotlib python-pip pkg-config cython python-zmq
 pip install ipython jinja2 tornado
 cd /vagrant; git clone git://github.com/ecell/ecell4; cd ecell4; git checkout -t origin/interface
 cd /vagrant/ecell4/core; python ../waf configure build install
